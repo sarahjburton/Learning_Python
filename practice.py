f11640f1c1707fb0ce9bc4482f5a6f565c8116f3
sarahjburton <sjburton@ucdavis.edu>
2022-08-02 12:27:30 -0700

    initial commit

--- practice.py
+++ practice.py
@@ -1,4 +1,14 @@
 import random
 
-r = random.randint(1-9)
-if r == 1: print 
+r = random.randint(1,9)
+print("Disneyland Attraction:")
+if r == 1: print('Matterhorn')
+elif r == 2: print("It's a Small World")
+elif r == 3: print("Space mMuntain")
+elif r == 4: print("Peter Pan")
+elif r == 5: print("Jungle Cruise")
+elif r == 6: print("Mr. Toad's Wild Ride")
+elif r == 7: print("Pirates of the Caribbean")
+elif r == 8: print("Teacups")
+elif r == 9: print("Big Thunder Mountain Railroad")
+
